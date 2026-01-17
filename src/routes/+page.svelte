68dd8689a144b6f323937383de913edd78a74766
Ruben Calero <rcalerocordoba@gmail.com>
2026-01-18 00:13:28 +0100

    2.0 - Funciones añadidas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,23 +10,46 @@
 	let ticket = [];
 	let total = 0;
 
+	function agregarAlTicket(producto) {
+		ticket.push(producto);
+		ticket = ticket;
+				
+        let suma = 0;
+		for (let i = 0; i < ticket.length; i = i + 1) {
+			suma = suma + ticket[i].precio;
+		}
+		total = suma;
+	}
+
+	function eliminarDelTicket(indice) {
+		ticket.splice(indice, 1);
+		ticket = ticket;
+		
+		let suma = 0;
+		for (let i = 0; i < ticket.length; i = i + 1) {
+			suma = suma + ticket[i].precio;
+		}
+		total = suma;
+	}
+
 </script>
 
 <h2>Frutería</h2>
 
 {#each productos as producto}
-	<button>
+	<button on:click={function() { agregarAlTicket(producto) }}>
 		{producto.nombre}
 	</button>
 {/each}
 
+<hr />
 
 <h3>Ticket</h3>
 <ul>
 	{#each ticket as producto, i}
 		<li>
 			{producto.nombre} - {producto.precio} €
-			<button>
+			<button on:click={function() { eliminarDelTicket(i) }}>
 				Eliminar
 			</button>
 		</li>
